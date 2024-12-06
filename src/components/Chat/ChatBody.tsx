3162edbe03bb7958b04c490aaff62d9921bd6a47
Onebluesky882 <119560471+Onebluesky882@users.noreply.github.com>
2024-12-06 18:12:38 +0700

    style : Chat

--- src/components/Chat/ChatBody.tsx
+++ src/components/Chat/ChatBody.tsx
@@ -6,7 +6,9 @@ export const ChatBody = () => {
       <div className={css["center"]}>
         {/* ---------------  friend  ------------------ */}
         <div className={css["message"]}>
-          <FaUserCircle size={100} />
+          <div>
+            <FaUserCircle size={20} />
+          </div>
           <div className={css["texts"]}>
             <p>
               It has survived not only five centuries, but also the leap into
@@ -32,34 +34,9 @@ export const ChatBody = () => {
         {/*  */}
         {/* ---------------  friend  ------------------ */}
         <div className={css["message"]}>
-          <FaUserCircle size={100} />
-          <div className={css["texts"]}>
-            <p>
-              It has survived not only five centuries, but also the leap into
-              electronic typesetting, remaining essentially unchanged. It was
-              publishing software like Aldus PageMaker including versions of
-              Lorem Ipsum.
-            </p>
-            <span> 1 min ago</span>
+          <div>
+            <FaUserCircle size={20} />
           </div>
-        </div>
-        {/*  */}
-        {/* ---------------  own  ------------------ */}
-        <div className={css["message-own"]}>
-          <div className={css["texts"]}>
-            <p>
-              It has survived not only five centuries, but also the leap into
-              electronic typesetting, remaining essentially unchanged. It was
-              publishing software like Aldus PageMaker including versions of
-              Lorem Ipsum.
-            </p>
-            <span> 1 min ago</span>
-          </div>
-        </div>
-        {/*  */}
-        {/* ---------------  friend  ------------------ */}
-        <div className={css["message"]}>
-          <FaUserCircle size={100} />
           <div className={css["texts"]}>
             <p>
               It has survived not only five centuries, but also the leap into
@@ -86,7 +63,38 @@ export const ChatBody = () => {
         {/*  */}
         {/* ---------------  friend  ------------------ */}
         <div className={css["message"]}>
-          <FaUserCircle size={100} />
+          <div>
+            <FaUserCircle size={20} />
+          </div>
+          <div className={css["texts"]}>
+            <p>
+              It has survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged. It was
+              publishing software like Aldus PageMaker including versions of
+              Lorem Ipsum.
+            </p>
+            <span> 1 min ago</span>
+          </div>
+        </div>
+        {/*  */}
+        {/* ---------------  own  ------------------ */}
+        <div className={css["message-own"]}>
+          <div className={css["texts"]}>
+            <p>
+              It has survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged. It was
+              publishing software like Aldus PageMaker including versions of
+              Lorem Ipsum.
+            </p>
+            <span> 1 min ago</span>
+          </div>
+        </div>
+        {/*  */}
+        {/* ---------------  friend  ------------------ */}
+        <div className={css["message"]}>
+          <div>
+            <FaUserCircle size={20} />
+          </div>
           <div className={css["texts"]}>
             <p>
               It has survived not only five centuries, but also the leap into
