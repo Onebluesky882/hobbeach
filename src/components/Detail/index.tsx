46cf30b71d94f092eaa5d7a0a5a3b984d9daa80b
Onebluesky882 <119560471+Onebluesky882@users.noreply.github.com>
2024-12-06 19:55:04 +0700

    create : detail

--- src/components/Detail/index.tsx
+++ src/components/Detail/index.tsx
@@ -1,5 +1,49 @@
-import "./style.css";
+import { FaUserCircle } from "react-icons/fa";
+import css from "./style.module.css";
+import { FaAngleDown } from "react-icons/fa";
 const Detail = () => {
-  return <div style={{ width: "800px" }}>detail</div>;
+  return (
+    <Container>
+      <div className={css["detail-profile"]}>
+        <div className={css[""]}>
+          <FaUserCircle size={40} />
+        </div>
+
+        <p>Owen Hargreaves</p>
+        <p className={css["description"]}>
+          Owen Lee Hargreaves (born 20 January 1981) is a former professional
+          footballer who played as a midfielder.
+        </p>
+      </div>
+
+      {/* body */}
+
+      <div className={css["info"]}>
+        <div className={css["option"]}>
+          <div className={css["title"]}>
+            <span>Chat setting</span>
+            <FaAngleDown />
+          </div>
+        </div>
+      </div>
+
+      <div className={css["option"]}>
+        <div className={css["title"]}>
+          <span>Photos</span>
+          <FaAngleDown />
+        </div>
+      </div>
+
+      {/*  */}
+      <div className={css["buttom-bar"]}>
+        <button className={css["button-block"]}>Block User</button>
+        <button className={css["button-logout"]}>Logout</button>
+      </div>
+    </Container>
+  );
+};
+
+const Container = ({ children }: React.PropsWithChildren) => {
+  return <div className={css["container"]}> {children}</div>;
 };
 export default Detail;
